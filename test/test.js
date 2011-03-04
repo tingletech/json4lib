4f7e099aa10da7d2efa6452b9f6a3f6eb1e665eb
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2011-03-04 07:37:25 -0800

    the tests are not really working...

--- test/test.js
+++ test/test.js
@@ -1,32 +1,54 @@
 // run as 'node test.js'
 
-fs = require('fs');
-JSV = require('JSV').JSV;
-env = JSV.createEnvironment("json-schema-draft-03");
+var fs = require('fs');
+var JSV = require('JSV').JSV;
+var env = JSV.createEnvironment("json-schema-draft-03");
 
 // read in our schemas
-schemas = fs.readdirSync('../schema/');
-schemasL = schemas.length;
+var schemas = fs.readdirSync('../schema/');
+var schemasL = schemas.length;
 
 for ( var i=0, len=schemasL; i<len; ++i ){
-  // console.log(schemas[i]);
+  console.log(schemas[i]);
   schema = fs.readFileSync("../schema/"+schemas[i], encoding='utf8');
   try {
     schema = JSON.parse(schema);
-    env.createSchema(schema);
+    var report = env.validate(schema);
+    if (report.errors.length === 0) {
+      console.log("JSON schema is valid against the schema");
+    } else {
+      console.log(report);
+      return false;
+    } 
+    env.createSchema(schema,true,"http://json4lib.googlecode.com/hg/"+schemas[i]+"#");
   } catch(SyntaxError) {
     console.log(SyntaxError);
     console.log(schema);
-    return false;
   }
 }
+console.log("load test object");
+var testObject = fs.readFileSync("./digital-object.json", encoding='utf8');
 
-testObject = fs.readFileSync("./digital-object.json", encoding='utf8');
+testObject = JSON.parse(testObject);
 
-report = env.validate(testObject);
+var report = env.validate(testObject);
 
 if (report.errors.length === 0) {
   console.log("JSON is valid against the schema");
 } else {
-  return false;
-}
+  console.log(report);
+} 
+
+console.log("load the incorrect object");
+var testObject = fs.readFileSync("./error.json", encoding='utf8');
+
+testObject = JSON.parse(testObject);
+
+var report = env.validate(testObject);
+
+if (report.errors.length === 0) {
+  console.log("JSON is valid against the schema");
+} else {
+  console.log(report);
+} 
+
