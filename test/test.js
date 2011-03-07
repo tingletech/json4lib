d6c57ec43b526e1f9191c690f3dd375afd90740b
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2011-03-06 17:47:42 -0800

    got the schemas validated against metaschema, but still can't get it to fail a test...

--- test/test.js
+++ test/test.js
@@ -4,8 +4,12 @@ var fs = require('fs');
 var JSV = require('JSV').JSV;
 var env = JSV.createEnvironment("json-schema-draft-03");
 
+
+
+
 // read in our schemas
-var schemas = fs.readdirSync('../schema/');
+// var schemas = fs.readdirSync('../schema/');
+var schemas = ['files.json','dcel.json','qdc.json','image-file.json','files.json','do.json'];
 var schemasL = schemas.length;
 
 for ( var i=0, len=schemasL; i<len; ++i ){
@@ -13,42 +17,43 @@ for ( var i=0, len=schemasL; i<len; ++i ){
   schema = fs.readFileSync("../schema/"+schemas[i], encoding='utf8');
   try {
     schema = JSON.parse(schema);
-    var report = env.validate(schema);
+    // var report = env.validate(schema);
+    var report = env.validate(schema, {"$ref":"http://json-schema.org/hyper-schema#"});
     if (report.errors.length === 0) {
       console.log("JSON schema is valid against the schema");
     } else {
       console.log(report);
       return false;
     } 
-    env.createSchema(schema,true,"http://json4lib.googlecode.com/hg/"+schemas[i]+"#");
+    env.createSchema(schema,true,"http://json4lib.googlecode.com/hg/schema/"+schemas[i]+"#");
+    console.log("http://json4lib.googlecode.com/hg/schema/"+schemas[i]+"#");
   } catch(SyntaxError) {
     console.log(SyntaxError);
     console.log(schema);
   }
 }
 console.log("load test object");
-var testObject = fs.readFileSync("./digital-object.json", encoding='utf8');
+var testObject1 = fs.readFileSync("./digital-object.json", encoding='utf8');
 
-testObject = JSON.parse(testObject);
+testObject1 = JSON.parse(testObject1);
 
-var report = env.validate(testObject);
+var report1 = env.validate(testObject1, {"$ref":"http://json4lib.googlecode.com/hg/schema/do.json#"});
 
-if (report.errors.length === 0) {
+if (report1.errors.length === 0) {
   console.log("JSON is valid against the schema");
 } else {
-  console.log(report);
+  console.log(report1);
 } 
 
 console.log("load the incorrect object");
-var testObject = fs.readFileSync("./error.json", encoding='utf8');
+var testObject2 = fs.readFileSync("./error.json", encoding='utf8');
 
-testObject = JSON.parse(testObject);
+testObject2 = JSON.parse(testObject2);
 
-var report = env.validate(testObject);
+var report2 = env.validate(testObject2, {"$ref":"http://json4lib.googlecode.com/hg/schema/do.json#"});
 
-if (report.errors.length === 0) {
+if (report2.errors.length === 0) {
   console.log("JSON is valid against the schema");
 } else {
-  console.log(report);
+  console.log(report2);
 } 
-
